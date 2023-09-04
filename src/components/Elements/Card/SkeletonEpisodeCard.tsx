a93e41e4d8608616c3c82728f5ee69ac21e13e40
yunho.jang <yunho.jang@wizcore.co.kr>
2023-09-04 18:46:21 +0900

    styles: epi skeleton size

--- src/components/Elements/Card/SkeletonEpisodeCard.tsx
+++ src/components/Elements/Card/SkeletonEpisodeCard.tsx
@@ -11,7 +11,7 @@ const SkeletonEpisodeCard: FC<SkeletonEpisodeCardProps> = () => {
     <div className="flex gap-3 cursor-pointer w-full">
       <Skeleton width="180px" height="110px" rounded="md" />
       <div className="flex flex-col flex-1 gap-1">
-        <Skeleton height="1.4rem" width="70%" rounded="md" />
+        <Skeleton height="1.3rem" width="50%" maxWidth={'200px'} rounded="md" />
         <Skeleton height="1.1rem" width="95%" rounded="md" />
         <Skeleton height="1.1rem" width="95%" rounded="md" />
       </div>
