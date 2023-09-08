0cfab3f56476927e5768644431f35a898167e295
yunho.jang <yunho.jang@wizcore.co.kr>
2023-09-08 14:09:51 +0900

    fe: init hover to show

--- src/features/contentModal/components/ContentToolBar.tsx
+++ src/features/contentModal/components/ContentToolBar.tsx
@@ -2,7 +2,7 @@ import { FC, HtmlHTMLAttributes, ReactNode } from 'react';
 import { AiOutlineUnorderedList } from 'react-icons/ai';
 import { IoClose } from 'react-icons/io5';
 import { useCoverNavigate } from '../hooks/useCoverNavigate';
-import { useNavigate, useParams } from 'react-router-dom';
+import ShowOnHover from './Elements/ShowOnHover';
 
 interface ContentToolBarProps {
   isEpisodePage?: boolean;
@@ -12,26 +12,28 @@ const ContentToolBar: FC<ContentToolBarProps> = ({ isEpisodePage }) => {
   const { coverClose, toContentList } = useCoverNavigate();
 
   return (
-    <div className="flex justify-between backdrop-blur-sm bg-white py-2.5 px-5 fixed top-0 left-0 w-full z-50 items-center">
-      <div className="flex gap-2">
-        {isEpisodePage ? (
-          <GoListButton onClick={toContentList} />
-        ) : (
-          <GoCloseButton onClick={coverClose} />
-        )}
-      </div>
+    <ShowOnHover initShow={false}>
+      <div className="flex justify-between backdrop-blur-sm bg-white py-2.5 px-5 fixed top-0 left-0 w-full z-50 items-center">
+        <div className="flex gap-2">
+          {isEpisodePage ? (
+            <GoListButton onClick={toContentList} />
+          ) : (
+            <GoCloseButton onClick={coverClose} />
+          )}
+        </div>
 
-      <div className="flex gap-1 items-center">
-        <>
-          <button className="p-2 px-5 cursor-pointer text-black font-bold">
-            이전화
-          </button>
-          <button className="p-2 px-5 cursor-pointer text-black font-bold">
-            다음화
-          </button>
-        </>
+        <div className="flex gap-1 items-center">
+          <>
+            <button className="p-2 px-5 cursor-pointer text-black font-bold">
+              이전화
+            </button>
+            <button className="p-2 px-5 cursor-pointer text-black font-bold">
+              다음화
+            </button>
+          </>
+        </div>
       </div>
-    </div>
+    </ShowOnHover>
   );
 };
 
--- src/features/contentModal/components/Elements/ShowOnHover.tsx
+++ src/features/contentModal/components/Elements/ShowOnHover.tsx
@@ -1,20 +1,38 @@
 import { useDisclosure } from '@/hooks/useClosure';
 import { LayoutProps } from '@/types';
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 
 interface ShowOnHoverProps extends LayoutProps {
   closeDelay?: number;
+  initShow?: boolean;
 }
 
-const ShowOnHover: FC<ShowOnHoverProps> = ({ children, closeDelay }) => {
-  const { isOpen: isShow, onOpen: open, onClose: hide } = useDisclosure();
+const ShowOnHover: FC<ShowOnHoverProps> = ({
+  children,
+  closeDelay = 2000,
+  initShow,
+}) => {
+  const {
+    isOpen: isShow,
+    onOpen: show,
+    onClose: hide,
+  } = useDisclosure(initShow);
+
+  const timer = useRef<NodeJS.Timer>(null);
+
+  const showWithDelayReset = () => {
+    show();
+  };
+  const hideWithDelay = () => {
+    // timer.current = setTimeout(() => hide(), closeDelay || 0);
+  };
 
   return (
     <div
       style={{ opacity: isShow ? 1 : 0 }}
-      onMouseEnter={open}
-      onMouseLeave={hide}
-      className="transition-opacity delay-75"
+      onMouseEnter={show}
+      onMouseLeave={hideWithDelay}
+      className="transition-opacity duration-75"
     >
       {children}
     </div>
